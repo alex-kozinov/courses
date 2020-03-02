827e534ddd19b0be37764416e6cee17c247c7f5e
Aleksey <kozinov.av@gmail.com>
2020-03-02 19:15:37 +0300

    add second homework

---
+++ practical-rl/02_value_based/mdp_get_action_value.py
@@ -0,0 +1,15 @@
+
+def get_action_value(mdp, state_values, state, action, gamma):
+    """ Computes Q(s,a) as in formula above """
+
+    q = 0
+    for next_state in mdp.get_next_states(state, action):
+        transition_prob = mdp.get_transition_prob(
+            state, action, next_state
+        )
+        reward = mdp.get_reward (
+            state, action, next_state
+        )
+        q += transition_prob * (reward + gamma*state_values[next_state])
+
+    return q
